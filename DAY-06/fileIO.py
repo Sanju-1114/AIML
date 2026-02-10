bbab94261f94f3dc6877e253e50309cfed59ddda
SANJEEV KUMAR <sanju20july2002@gmail.com>
2026-02-10 22:08:47 +0530

    first commit

---
+++ DAY-06/fileIO.py
@@ -0,0 +1,13 @@
+# f = open("sample.txt", "r")
+f = open("sample.txt", "w")
+
+# data = f.read()
+# data = f.readline()
+# print(data)
+
+# data = f.readline()
+# print(data)
+
+f.write("I'm Sanju. \nWhat's your name?")
+
+f.close()
