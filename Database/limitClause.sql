183d52c92b47bbf88b6a1350859b4c605b12684e
SANJEEV KUMAR <sanju20july2002@gmail.com>
2026-02-26 10:22:11 +0530

    Limit cluase in SQL

---
+++ Database/limitClause.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE instagram;
+CREATE DATABASE IF NOT EXISTS instagram;
+
+USE instagram;
+
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    age INT,
+    user_name VARCHAR(30) NOT NULL,
+    email VARCHAR(30) UNIQUE,
+    followers INT DEFAULT 0,
+    followings INT,
+    CONSTRAINT CHECK (age >= 13)
+);
+
+INSERT INTO users
+(id, age, user_name, email, followers, followings)
+VALUES
+(1, 14, "Adam", "adam@gmail.com", 123, 145),
+(2, 15, "Bob", "bob@gmail.com", 200, 200),
+(3, 16, "Casey", "casey@gmail.com", 300, 306),
+(4, 17, "Donald", "donald@gmail.com", 200, 105);
+(5, 14, "Eve", "eve@gmail.com", 400, 145),
+(6, 16, "Farah", "farah@gmail.com", 10000, 1000);
+
+SELECT * FROM users
+LIMIT 3;
+
+SELECT user_name, age, email 
+FROM users
+WHERE age > 14
+LIMIT 2;
