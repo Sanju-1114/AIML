44b3d23d7989f4d4debfe60181ce23c31d4015e5
SANJEEV KUMAR <sanju20july2002@gmail.com>
2026-02-26 10:21:28 +0530

    Where cluase in SQL

---
+++ Database/whereClause.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE instagram;
+CREATE DATABASE IF NOT EXISTS instagram;
+
+USE instagram;
+
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    age INT,
+    user_name VARCHAR(30) NOT NULL,
+    email VARCHAR(30) UNIQUE,
+    followers INT DEFAULT 0,
+    followings INT,
+    CONSTRAINT CHECK (age >= 13)
+);
+
+INSERT INTO users
+(id, age, user_name, email, followers, followings)
+VALUES
+(1, 14, "Adam", "adam@gmail.com", 123, 145),
+(2, 15, "Bob", "bob@gmail.com", 200, 200),
+(3, 16, "Casey", "casey@gmail.com", 300, 306),
+(4, 17, "Donald", "donald@gmail.com", 200, 105);
+
+SELECT * 
+FROM users
+WHERE followers >= 200;
+
+SELECT user_name, age 
+FROM users
+WHERE age + 1 = 18;
+
+SELECT user_name, age 
+FROM users
+WHERE age < 16;
