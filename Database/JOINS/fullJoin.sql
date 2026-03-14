de9d3696108b7e32390013e1cb263203a04676a4
SANJEEV KUMAR <sanju20july2002@gmail.com>
2026-03-14 11:56:15 +0530

    full Join in SQL

---
+++ Database/JOINS/fullJoin.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE prime;
+USE prime;
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    city VARCHAR(50)
+);
+
+INSERT INTO customers VALUES
+(1, "Alice", "Mumbai"),
+(2, "Bob", "Delhi"),
+(3, "Charlie", "Bangalore"),
+(4, "David", "Mumbai");
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    amount INT
+);
+
+INSERT INTO orders VALUES
+(101, 1, 500),
+(102, 1, 900),
+(103, 2, 300),
+(104, 5, 700);
+
+
+SELECT * FROM customers;
+SELECT * FROM orders;
+
+
+-- full join
+SELECT * FROM customers as c
+LEFT JOIN orders as o
+ON c.customer_id = o.customer_id
+UNION
+SELECT * FROM customers as c
+RIGHT JOIN orders as o
+ON c.customer_id = o.customer_id;
+
