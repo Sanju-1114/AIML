6d92d5350244a829becf86d1119319184ba811d2
SANJEEV KUMAR <sanju20july2002@gmail.com>
2026-03-14 11:55:34 +0530

    left Join in SQL

---
+++ Database/JOINS/leftJoin.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE prime;
+USE prime;
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    city VARCHAR(50)
+);
+
+INSERT INTO customers VALUES
+(1, "Alice", "Mumbai"),
+(2, "Bob", "Delhi"),
+(3, "Charlie", "Bangalore"),
+(4, "David", "Mumbai");
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    amount INT
+);
+
+INSERT INTO orders VALUES
+(101, 1, 500),
+(102, 1, 900),
+(103, 2, 300),
+(104, 5, 700);
+
+
+SELECT * FROM customers;
+SELECT * FROM orders;
+
+
+-- left join
+SELECT *
+FROM customers c
+LEFT JOIN orders o
+ON c.customer_id = o.customer_id;
+
+
