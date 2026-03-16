d2dbd173c85a9770eb229070bc87cb9f9f854fd2
SANJEEV KUMAR <sanju20july2002@gmail.com>
2026-03-16 10:07:53 +0530

    indexes in SQL

---
+++ Database/indexes.sql
@@ -0,0 +1,26 @@
+CREATE TABLE accounts (
+    account_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    balance DECIMAL(10, 2),
+    branch VARCHAR(50)
+);
+
+INSERT INTO accounts
+VALUES
+(1, "Adam", 500.00, "Mumbai"),
+(2, "Bob", 300.00, "Delhi"),
+(3, "Charlie", 700.00, "Bangalore"),
+(4, "David", 1000.00, "Noida");
+
+SELECT * FROM accounts;
+
+CREATE INDEX idx_branch ON accounts(branch);
+
+SHOW INDEX FROM accounts;
+
+SELECT * 
+FROM accounts
+WHERE branch="Mumbai";
+
+
+CREATE INDEX idx2 ON accounts(branch, balance);
